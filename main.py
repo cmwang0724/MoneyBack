2cfb31088cf7c95e5a43efa5c6c597872962fc35
guangxing.chen <chengx168000@outlook.com>
2013-07-08 11:26:59 +0800

    Transfer to Eclipse with PyDev

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import web
 
 urls= (
-	'/', 'index'
+	'/', 'home',
+	'/(.*)/', 'redirect'
 )
 
-class index:
+class home:
 	def GET(self):
-		return "Hey, world!"
+		return "Hey, world!!!"
+
+class redirect:
+	def GET(self, path):
+		web.seeother('/' + path)
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
