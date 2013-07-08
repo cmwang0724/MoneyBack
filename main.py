624d81d93c9d3bd8cddb461d5809b406752d3bc1
guangxing.chen <chengx168000@outlook.com>
2013-07-08 18:03:54 +0800

    Create custom mb module for money back.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import web
-
-urls= (
-	'/', 'home',
-	'/(.*)/', 'redirect'
-)
+import mb
 
 class home:
 	def GET(self):
-		return "Hey, world!!!"
+		render = web.template.render('templates/', base='layout')
+		return render.test()
+#		return "Hey, world!!! I am back!!!"
 
 class redirect:
 	def GET(self, path):
 		web.seeother('/' + path)
 
 if __name__ == "__main__":
-	app = web.application(urls, globals())
+	app = web.application(mb.conf.urls, globals())
 	app.run()
---
+++ mb/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""mb.py: money back web apps (http://...)"""
+
+__version__ = "1.0.0"
+__author__ = [
+    "Carl Chen <chengx168000@outlook.com>"
+]
+
+import conf
+
+from conf import *
